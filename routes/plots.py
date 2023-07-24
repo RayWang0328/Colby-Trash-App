184c77aa78759e5b03fa991a0760735fe1d4b001
Ray Wang <raywang0328@gmail.com>
2023-07-24 12:55:38 -0400

    Added markers for plots

--- routes/plots.py
+++ routes/plots.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import pandas as pd
+import folium
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 import seaborn as sns
@@ -32,14 +33,32 @@ def plots():
     images = []
 
     for i in range(num_clusters):
-       # plt.figure(figsize=(10, 5))
         sns.countplot(x='type', data=df[df['cluster']==i], palette=color_dict)
-        plt.title(f'Frequency Plot for Cluster {i+1}')
+        plt.title(f'Frequency Plot for Cluster {i}')
         sio = BytesIO()
         plt.savefig(sio, format='png')
         plt.close()
         sio.seek(0)
         image = base64.b64encode(sio.read()).decode('utf-8')
         images.append(image)
+
+    # reverse scale transformation to get the original coordinates
+    df[['longitude', 'latitude']] = scaler.inverse_transform(df[['longitude', 'latitude']])
+
+    # calculate cluster centers
+    cluster_centers = df.groupby('cluster')[['longitude', 'latitude']].mean().reset_index()
+
+    # initialize the map with the first cluster's coordinates
+
+    cluster_map = python.config.map
+    # add a marker for each cluster center
+    for _, row in cluster_centers.iterrows():
+        folium.Marker(
+            location=[row['longitude'], row['latitude']],
+            popup=f"Cluster {row['cluster']}"
+        ).add_to(cluster_map)
+
+    # save the map as HTML
+    python.config.map = cluster_map
 
     return jsonify({'images': images}), 200
